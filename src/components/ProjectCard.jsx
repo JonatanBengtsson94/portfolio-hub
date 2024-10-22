05180d8d03e02cb26f279677ae09371c8769e31f
jb <jb@KBC143.home>
2024-10-22 21:55:49 +0200

    created ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,12 @@
 function ProjectCard(props) {
   return (
-    <h1>{props.title}</h1>
+    <a href={props.url} target="_blank" rel="noopener noreferrer" className="project-card">
+      <img src={`src/assets/${props.image}`} />
+      <div className="overlay">
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+      </div>
+    </a>
   )
 }
 
