cd4b769a70845dbcc27350d4d9b7f7b09c2084a2
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-10-29 14:29:34 +0100

    added about and resume

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+function Experience(props) {
+  return (
+    <div>
+      <h4>{props.title}</h4>
+      <h5>{props.orginization}</h5>
+      <h5>{props.date}</h5>
+      <p>{props.description}</p>
+    </div>
+  )
+}
+
+export default Experience
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,13 @@
+import About from "./About.jsx"
 import ProjectGallery from "./ProjectGallery.jsx"
 
 function Welcome() {
   return (
   <main>
-      <section id="about"></section>
+      <section id="about">
+        <h2>About</h2>
+        <About />
+      </section>
       <section id="projects">
         <h2>Projects</h2>
         <ProjectGallery />
