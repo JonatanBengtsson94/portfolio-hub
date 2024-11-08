8d3aa3a32b091f557eb4876ed5174a343c549a0f
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-11-08 12:43:35 +0100

    import images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,12 @@
 import "./ProjectCard.css"
 
 function ProjectCard(props) {
+  const imageUrl = new URL(`../assets/${props.image}`, import.meta.url).href;
+
   return (
     <div className="card">
       <a href={props.url} target="_blank" rel="noopener noreferrer" className="project-card">
-        <img src={`src/assets/${props.image}`} />
+        <img src={imageUrl} />
         <div className="overlay">
           <h1>{props.title}</h1>
           <p>{props.description}</p>
