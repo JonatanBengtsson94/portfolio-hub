5332821bb562168a5910f6c15c84bad2a75447cc
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-11-08 20:04:27 +0100

    reduced about information

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,3 @@
-import About from "./About.jsx"
 import ProjectGallery from "./ProjectGallery.jsx"
 
 function Welcome() {
@@ -6,10 +5,18 @@ function Welcome() {
   <main>
       <section id="about">
         <h2>About</h2>
-        <About />
+        <p>
+          Hello, I am Jonatan. Welcome to my personal portfolio site! Here you can find my latest projects and resume.
+          I am system developer with a strong foundation in Geographic Information Systems (GIS).
+          A passionate technology enthusiast with a particular affinity for Linux, I thrive on exploring new tools and methodologies to improve processes. 
+          Committed to continuous learning, I seek opportunities to contribute to impactful projects in a collaborative environment.
+        </p>
       </section>
       <section id="projects">
         <h2>Projects</h2>
+        <p>
+          A collection of hobby projects and experiements. 
+        </p>
         <ProjectGallery />
       </section>
       <section id="contact"></section>
